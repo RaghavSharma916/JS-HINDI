f9841416303ad203247fb038715c0bde739bc780
RaghavSharma916 <raghavsharma.27.r@gmail.com>
2026-01-24 11:48:22 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version
+//alert(3+3)// we are using nodejs not browser
+
+console.log(3
+    +
+    3) // code readability should be high  
+
+console.log("Abc")
+
+
+let name="raghav"
+let age=21
+let isLoggedIn=false
+let state = null;
+//number =>2 to power 53
+//bigint 
+//string => ""
+//boolean=> true/false
+// null => standalone value
+//undefined => 
+// symbol => unique
+
+// Object
+
+console.log(typeof null);
