9914d2b93b27b8a5c8c1bf985d3a8cec718ed30b
RaghavSharma916 <raghavsharma.27.r@gmail.com>
2026-01-24 20:00:06 +0000

    extend

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -3,3 +3,17 @@ const anotherId=Symbol('121')
 //console.log(id === anotherId)
 const heros=["a","b"]
 console.log(typeof heros)
+
+// +++++++++++++++++++
+//  Stack(primitive), Heap(non primitive)
+let userOne={
+    email:"user@",
+    upi:"user@ybl"
+
+}
+let usertwo=userOne
+
+usertwo.email="my@"
+console.log(userOne.email);
+console.log(usertwo.email)
+
