9a27da7ed29e9381b235f5a5baaa0afd384d9fc1
RaghavSharma916 <raghavsharma.27.r@gmail.com>
2026-01-24 11:22:06 +0000

    change

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail="raghav@gmail.com"
+var accountPassword="123456"
+accountCity="jaipur"
+let accountState;
+//accountId=2//not allowed
+
+accountEmail="r@gmail.com";
+accountPassword="2121"
+accountCity="banglore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountState])
+
