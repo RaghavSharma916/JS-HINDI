573c4115859107bcead4d094c9f27a9c30feb7dc
RaghavSharma916 <raghavsharma.27.r@gmail.com>
2026-01-26 09:34:06 +0000

    nums

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,26 @@
+const name="raghav"
+const repocount=50
+
+//console.log(name + repocount+" value"+ 78)
+ 
+//console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+const gameName=new String('abacdef')
+console.log(gameName[0]);
+
+/*console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+
+console.log(name.indexOf('a'));
+const newString=gameName.substring(0,2)
+console.log(newString);*/
+
+const anotherString=name.slice(-4,5)
+console.log(anotherString)
+
+const newStringOne="  raghav  "
+console.log(newStringOne);
+console.log(newStringOne.trim())
+
+const url="https://raghav.com/ragahv%20%"
+console.log(url.replace('%20','-'));
